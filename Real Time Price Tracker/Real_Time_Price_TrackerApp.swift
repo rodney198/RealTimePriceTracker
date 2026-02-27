c1c228ce240caa5bc089d161aa71d7e71da9c392
rodney198 <rodneysfit2020@gmail.com>
2026-02-27 08:19:17 +0530

    Initial commit with README

---
+++ Real Time Price Tracker/Real_Time_Price_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_Time_Price_TrackerApp.swift
+//  Real Time Price Tracker
+//
+//  Created by Rodney Pinto on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Real_Time_Price_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
